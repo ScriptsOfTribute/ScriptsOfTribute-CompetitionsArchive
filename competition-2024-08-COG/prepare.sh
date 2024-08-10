b41e6458fdc84a9b6c0c1fc141ff02bcc1a52f94
Radosław Miernik <radekmie@gmail.com>
2024-08-10 18:01:06 +0200

    Removed temporary ToT-BoT logs.

--- competition-2024-08-COG/prepare.sh
+++ competition-2024-08-COG/prepare.sh
@@ -36,5 +36,5 @@ cp ../GameRunner/bin/Release/net7.0/cards.json .
 
 # Start ToT-BoT server.
 kill $(lsof -t -i:12345)
-nohup python3 ToT-BoT/RLTraining/sot_rl_environment.py > ToT-BoT-output-4.log 2>&1 &
+nohup python3 ToT-BoT/RLTraining/sot_rl_environment.py > ToT-BoT.log 2>&1 &
 disown
